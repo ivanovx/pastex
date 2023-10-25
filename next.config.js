0fe4558f8ded6ef5a1743cb951ac240840e2724e
Ivan Ivanov <me@ivanov.pro>
2023-10-25 14:50:41 +0300

    common updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const CopyPlugin = require(`copy-webpack-plugin`)
-const withBundleAnalyzer = require(`@next/bundle-analyzer`)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
