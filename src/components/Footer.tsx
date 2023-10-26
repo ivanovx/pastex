8b9f1bef25490ccaf688a8cf1c0be92c22766806
Ivan Ivanov <me@ivanov.pro>
2023-10-26 12:10:39 +0300

    common updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,19 @@
-import { Stack, Typography } from "@mui/material"
-import { NextLink } from "./NextLink"
-import GithubIcon from "@mui/icons-material/GitHub"
+import { Stack } from "@mui/material";
+import { NextLink } from "./NextLink";
 
 export const Footer = () => (
-  <Stack
-    justifySelf="flex-end"
-    component="footer"
-    direction="row"
-    justifyContent="space-evenly"
-    padding="1rem"
-    flexWrap="wrap"
-    gap={1}
-    borderTop="1px dashed white"
-  >
-    <Typography>Powered by NextJS, Postgres and AWS</Typography>
-    <Stack direction="row" gap={1}>
-      <NextLink color="inherit" href="https://github.com/KonstantinSimeonov">
-        Konstantin Simeonov
-      </NextLink>{" "}
-      /
-      <NextLink
-        color="inherit"
-        href="https://github.com/KonstantinSimeonov/pastes"
-      >
-        <Typography>pastes</Typography>
-      </NextLink>
-      <GithubIcon />
+    <Stack
+        justifySelf="flex-end"
+        component="footer"
+        direction="row"
+        justifyContent="space-evenly"
+        padding="1rem"
+        flexWrap="wrap"
+        gap={1}
+        borderTop="1px dashed white"
+    >
+        <Stack direction="row" gap={1}>
+            (c) 2023 <NextLink color="inherit" href="https://www.ivanov.pro">Ivan Ivanov</NextLink>
+        </Stack>
     </Stack>
-  </Stack>
-)
+);
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,28 +1,27 @@
-import { Box, Typography } from "@mui/material"
-import { Stack } from "@mui/system"
-import React from "react"
-//import { Footer } from "./Footer"
-import { MostRecent } from "./MostRecent"
-import { Nav } from "./Nav"
+import React from "react";
+import { Stack } from "@mui/system";
+import { Box, Typography } from "@mui/material";
 
-export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return (
+import { Nav } from "./Nav";
+import { Footer } from "./Footer";
+import { MostRecent } from "./MostRecent";
+
+export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => (
     <Stack minHeight="100%" maxHeight="100vh">
-      <Nav />
-      <Stack direction="row" flexWrap="wrap" sx={{ overflow: `auto` }}>
-        <Box component="main" style={{ flexGrow: 1, padding: `1rem` }}>
-          {children}
-        </Box>
-        <Stack component="aside" sx={{ minWidth: `18%` }}>
-          <Box sx={{ padding: `1rem` }}>
-            <Typography variant="h5" component="h2">
-              Most recent pastes
-            </Typography>
-          </Box>
-          <MostRecent />
+        <Nav />
+        <Stack direction="row" flexWrap="wrap" sx={{ overflow: `auto` }}>
+            <Box component="main" style={{ flexGrow: 1, padding: `1rem` }}>
+                {children}
+            </Box>
+            <Stack component="aside" sx={{ minWidth: `18%` }}>
+                <Box sx={{ padding: `1rem` }}>
+                    <Typography variant="h5" component="h2">
+                        Most recent pastes
+                    </Typography>
+                </Box>
+                <MostRecent />
+            </Stack>
         </Stack>
-      </Stack>
-         </Stack>
-  ) //  <Footer />
-
-}
+        <Footer />
+    </Stack>
+);
